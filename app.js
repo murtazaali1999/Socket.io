c78a7207ee5f9b60470e51b19caf5d460346c8c4
Murtaza Ali <murtazaalimashadi1999@gmail.com>
2022-01-06 01:44:35 +0500

    file handling with stream

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const fs = require("fs"); //file system
 
+const writeStream = fs.WriteStream("./noteFolder/note.txt");
 const readStream = fs.ReadStream("./noteFolder/note.txt");
 
 readStream.on("data", (chunk) => {
   console.log(chunk.toString());
 });
+
+writeStream.write("my hand writing");
